1df1c486dff8c658b6d9c9f472c147752b1f1310
Garvit2003 <garvit.joshi.min21@itbhu.ac.in>
2023-07-04 23:29:28 +0530

    BFS

---
+++ Week-19(Graphs-1)/Lecture-1/A-6_BFS.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <list>
+#include <queue>
+using namespace std;
+template <typename T>
+
+class Graph
+{
+public:
+    unordered_map<T, list<T>> adjList;
+
+    // how to add edge in a list
+    void addEdge(T u, T v, bool direction)
+    {
+        // direction=0->undirected graph
+        // direction=1->directed graph
+        // create an edge from u to v
+        adjList[u].push_back(v);
+        if (direction == 0)
+        {
+            adjList[v].push_back(u);
+        }
+    }
+
+    void printAdjacencyList()
+    {
+        for (auto node : adjList)
+        {
+            cout << node.first << "->";
+            for (auto neighbour : node.second)
+            {
+                cout << neighbour << ",";
+            }
+            cout << endl;
+        }
+    }
+
+    void bfs(T src)
+    {
+        queue<T> q;
+        unordered_map<T, bool> visited;
+
+        q.push(src);
+        visited[src] = true;
+
+        while (!q.empty())
+        {
+            int frontnode = q.front();
+            q.pop();
+            cout << frontnode << ",";
+
+            // insert neighbours
+            for (auto neighbour : adjList[frontnode])
+            {
+                if (!visited[neighbour])
+                {
+                    q.push(neighbour);
+                    visited[neighbour] = true;
+                }
+            }
+        }
+    }
+};
+
+int main()
+{
+    // int n;
+    // cout << "Enter the number of nodes: ";
+    // cin >> n;
+
+    // vector<vector<int>> adj(n, vector<int>(n, 0));
+
+    // int e;
+    // cout << "Enter the number of edges: ";
+    // cin >> e;
+
+    Graph<int> g;
+    g.addEdge(0, 1, 0);
+    g.addEdge(1, 2, 0);
+    g.addEdge(1, 3, 0);
+    g.addEdge(3, 5, 0);
+    g.addEdge(3, 7, 0);
+    g.addEdge(7, 6, 0);
+    g.addEdge(7, 4, 0);
+    g.printAdjacencyList();
+}
