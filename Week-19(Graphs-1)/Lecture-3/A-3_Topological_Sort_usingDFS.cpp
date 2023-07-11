f3081049bbad0772586e9f5da94269f80f514d8b
Garvit2003 <garvit.joshi.min21@itbhu.ac.in>
2023-07-11 23:59:35 +0530

    Topological Sort

--- Week-19(Graphs-1)/Lecture-3/A-3_Topological_Sort_usingDFS.cpp
+++ Week-19(Graphs-1)/Lecture-3/A-3_Topological_Sort_usingDFS.cpp
@@ -33,4 +33,55 @@ public:
             cout << endl;
         }
     }
+
+    void dfs(T src, unordered_map<int, bool> &visited)
+    {
+        cout << src << ",";
+        visited[src] = true;
+
+        for (auto neighbour : adjList[src])
+        {
+            if (!visited[neighbour])
+            {
+                dfs(neighbour, visited);
+            }
+        }
+    }
+
+    void topoSortDFS(T src, unordered_map<int, bool> &visited, stack<int> &ans)
+    {
+
+        visited[src] = true;
+
+        for (auto neighbour : adjList[src])
+        {
+            if (!visited[neighbour])
+            {
+                dfs(neighbour, visited);
+            }
+        }
+        // while returning store the node in stack
+        ans.push(src);
+    }
+}
+
+int
+main()
+{
+    unordered_map<int, bool> visited;
+    stack<int> ans;
+    for (int i = 0; i < n; i++)
+    {
+        if (!visited[i])
+        {
+            g.topoSortDFS(i, visited, ans);
+        }
+    }
+
+    cout << "topo sort" << endl;
+    while (!ans.empty())
+    {
+        cout << ans.top();
+        ans.pop();
+    }
 }
